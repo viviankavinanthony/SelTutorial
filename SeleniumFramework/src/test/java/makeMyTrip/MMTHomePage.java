a774c30a2f668049d76f7d0e4c6ea402b0b2f749
viviankavinanthony <39612670+viviankavinanthony@users.noreply.github.com>
2019-08-06 20:02:38 +0530

    Final

--- SeleniumFramework/src/test/java/makeMyTrip/MMTHomePage.java
+++ SeleniumFramework/src/test/java/makeMyTrip/MMTHomePage.java
@@ -13,21 +13,28 @@ public class MMTHomePage {
 		
 		driver.get("https://www.goindigo.in/");
 		
+		//Passenger increment and decrement
 		driver.findElement(By.xpath("//*[@id=\'bookFlightTab\']/form/div[5]/div[1]/div[1]")).click();
-		
 		Thread.sleep(2000);
-		
 		for(int i=1; i<5;i++)
 		{
 			driver.findElement(By.xpath("//*[@id='bookFlightTab']/form/div[5]/div[1]/div[2]/ul/li[1]/div/button[2]")).click();
 			Thread.sleep(1000);
 		}
-		
 		Thread.sleep(2000);
-		
 		driver.findElement(By.xpath("//*[@id='bookFlightTab']/form/div[5]/div[1]/div[2]/div/button")).click();
 		
+		//From dropdown
+		driver.findElement(By.className("form-control or-src-city parsley-success")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//div[contains(text(),'Chhatrapati Shivaji')])[1]")).click();
+		Thread.sleep(1000);
 		
+		//To Dropdown
+		driver.findElement(By.className("form-control or-dest-city parsley-success")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//div[contains(text(),'Indira Gandhi')])[3]")).click();
+		Thread.sleep(1000);
 		
 		driver.quit();
 
